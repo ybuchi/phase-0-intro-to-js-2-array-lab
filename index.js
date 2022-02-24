21abc634239d07f5e8dbac5c9b7e20ad06d2c9c3
ybuchi <yabuch_ch@hotmail.com>
2022-02-23 22:05:55 -0500

    commit tree 47f0f0e854a896e081ec0ca1a2717d188749c906

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+};
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+};
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+};
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+};
+
+function appendCat(name){
+    let newCats = [...cats, "Broom"];
+    return newCats;
+};
+
+function prependCat(name){
+    let newCats = ["Arnold", ...cats];
+    return newCats;
+};
+
+function removeLastCat(){
+    let newCats = cats.slice(0,2);
+    return newCats;
+};
+
+function removeFirstCat(){
+    let newCats = cats.slice(1,3);
+    return newCats;
+};
+
+console.log(removeFirstCat());
+
+
+
+
